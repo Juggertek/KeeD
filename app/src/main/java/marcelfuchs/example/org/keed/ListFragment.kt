f1285f3d6ebb518ce38321f2abab27b04548b573
Marcel Fuchs <68725691+Juggertek@users.noreply.github.com>
2020-09-03 11:02:02 +0100

    ViewBinding komplett implementiert, SwipeToDelete.kt muss noch gefixt werden.

--- app/src/main/java/marcelfuchs/example/org/keed/ListFragment.kt
+++ app/src/main/java/marcelfuchs/example/org/keed/ListFragment.kt
@@ -2,7 +2,6 @@ package marcelfuchs.example.org.keed
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -13,12 +12,10 @@ import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_list.*
 import marcelfuchs.example.org.keed.databinding.FragmentListBinding
 
 lateinit var myAdapter: RecyclerAdapter
-//private lateinit var viewModel: ListViewModel
 
 class MainListFragment : Fragment() {
 
@@ -26,8 +23,7 @@ class MainListFragment : Fragment() {
 
     private var _binding: FragmentListBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
 
@@ -36,7 +32,6 @@ class MainListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        viewModel = ViewModelProvider(this).get(ListViewModel::class.java)
 
         _binding = DataBindingUtil.inflate(inflater, R.layout.fragment_list, container, false)
         return binding.root
@@ -53,7 +48,7 @@ class MainListFragment : Fragment() {
 
         // close the softKeyboard as it keeps open when returning from NewItemFragment
         val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(requireView().getWindowToken(), 0)
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
 
         val itemTouchHelper = ItemTouchHelper(SwipeToDelete())
         itemTouchHelper.attachToRecyclerView(rv_killsDeaths)
